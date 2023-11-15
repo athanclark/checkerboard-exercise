824e098a5fea91bbd0b8a6c274ae6727846b186f
Athan Clark <athan.clark@gmail.com>
2023-11-15 11:37:29 -0500

    random colored checkers

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ for (let i = 0; i < 8; i++) {
         const block = document.createElement('div');
         block.style.width = '12.5%';
         block.style.height = '12.5%';
-        block.style.background = (i % 2) ^ (j % 2) === 0 ? 'red' : 'black';
+        // block.style.background = (i % 2) ^ (j % 2) === 0 ? 'red' : 'black';
+        let sample = Math.floor(Math.random() * 256 * 256 * 256).toString(16);
+        sample = '0'.repeat(6 - sample.length) + sample; // left-pad 0 characters
+        block.style.background = `#${sample}`;
         container.appendChild(block);
     }
 }
