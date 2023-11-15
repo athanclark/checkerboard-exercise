d55c49573edcf299a9a2fb19d56a1371789f04ec
Athan Clark <athan.clark@gmail.com>
2023-11-15 12:02:18 -0500

    cool gradient

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 container.style.width = '800px';
 container.style.height = '800px';
+container.style.background = 'linear-gradient(135deg, #2fd7e8, #e07a8c)';
 
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
         const block = document.createElement('div');
         block.style.width = '12.5%';
         block.style.height = '12.5%';
-        // block.style.background = (i % 2) ^ (j % 2) === 0 ? 'red' : 'black';
-        let sample = Math.floor(Math.random() * 256 * 256 * 256).toString(16);
-        sample = '0'.repeat(6 - sample.length) + sample; // left-pad 0 characters
-        block.style.background = `#${sample}`;
+        if ((i % 2) ^ (j % 2) === 0) {
+            const start = `rgba(46.7, 146.9, 231.5, ${((i + j) / 14)})`;
+            const stop = `rgba(216.0, 91.3, 166.5, ${1 - ((i + j) / 14)})`;
+            block.style.background =
+                `linear-gradient(135deg, ${start}, ${stop})`;
+        }
         container.appendChild(block);
     }
 }
